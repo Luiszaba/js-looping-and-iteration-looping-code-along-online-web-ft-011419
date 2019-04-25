5b0fd8b8a90aa3b94c907f2b13ce3d82bed7eb10
Luis Zabala <luis.zabala076@gmail.com>
2019-04-25 00:29:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,21 +6,32 @@ const badges = ['Ada', 'Brendan', 'Ali'];
 
 // we create a function called printBadges with our badges variable passed in.  
 function printBadges (badges) {
-// create a for loop within the function.  Here we have a variable (let) equal to 0
+// create a for loop within the function.  Here we have a let variable equal to 0.  the 0 is the starting point for our counter. As long as i is less than the length of the badges array, the function will continue to loop and increase the counter console.log().  i++ signifies a positive +1 increment for every loop.
   for (let i = 0; i < badges.length; i++) {
     console.log('Welcome #${badges[i]}, You are employee #${i + 1).');
   }
+// we return the completed for loop as a string
   return badges;
 }
 
+// the function tailsNeverFails asserts the assumption that there is a .50 chance tails will not fail.  A coin only has who sides.  
 function tailsNeverFails () {
 // counter [i] = 0
   let i = 0;
 
-// while random num is equal to or greater than .5 increase the counter.
+// [key word] => 'while' a random num is equal to or greater than .5 increase the counter.
   while (Math.random() >= 0.5) {
     i++;
   }
 // return the counter total
   return `You got ${i} tails in a row!`;
 }
+
+
+// function printBadges(badges) {
+//  for (let i= 0, i < badges.length; i++) {
+//  console.log("Welcome, #${badges}, You are employee #${i + 1}");
+// }
+// return badges;
+// }
+// 
